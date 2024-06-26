bf66e21cfa87965abe9e44c85acc38fa8c1ec027
shafrnv <shafrnv@gmail.com>
2024-06-26 18:41:13 +0300

    Initial commit

---
+++ app/src/main/java/com/example/appforguap/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.example.appforguap
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        /*!1.Это нужно, чтобы было красивое приложение и статус бар с навигацией не выглядили
+        страшно и встраивались в приложение*/
+        enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        //!1.Вот до этого момента
+    }
+}
